e734a2c22997ce469036ea73d39771594fb491f9
rmr <ryan.raasch@gmail.com>
2021-08-30 11:19:09 +0200

    Dont bother with rmmod. The lifecycle of the pod will not dynamically unload drivers.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-modprobe -S ${KVER} dfl-pci
+
+modules=$(cat /lib/modules/$(uname -r)/modules.dep | cut -d : -f 1 | cut -d . -f 1)
+
+for module in $modules ; do
+    modprobe $module
+done
 
 sleep infinity
